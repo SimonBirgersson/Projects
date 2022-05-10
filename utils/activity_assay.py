180324906e312bafafc81bb8242f6ce290f3e008
Simons laptop <simon.w.birgersson@gmail.com>
2022-05-10 14:43:07 +0200

    leaning jupyter

--- utils/activity_assay.py
+++ utils/activity_assay.py
@@ -21,6 +21,14 @@ def estimate(abs, slope, intercept):
 class Absorbance:
     """
     Parent class that allows numpy arrays of absorbance data with absorbance mean and std
+
+    INPUT:
+        abs is the absorbance values that were measured in each cuvette ordered in a numpy array (matrix-like), default is that the each column is a unique experiment and each row of that column is the amount of replicates.
+
+        shape is a modifier to how the numpy array is read, rows switches experiments and replicates instead.
+
+    OUTPUT:
+        calculates mean absorbance and its standards deviation as self.mean and self.std
     """
 
     def __init__(
@@ -46,18 +54,23 @@ class Standard(Absorbance):
     """
     child Class for std data. Provides method for fitting, plotting and showing data.
 
-    INPUT:
-    absorbance: numpy array of your absorbance data, default is experiments in rows, replicates in col.
-    concentration: numpy array of your known std concentration
-    x_unit: str of the unit of concentration
-    y_unit: str of unit for signal, default is absorbance.
-    shape: modifies the order of experiments and replicates.
+    input:
+        absorbance: numpy array of your absorbance data, default is experiments in rows, replicates in col.
 
-    self.fit is a regression object (see scipy -> linreg for further details)
+        concentration: numpy array of your known std concentration
 
-    OUTPUT:
-    printing the class shows matrix of data
-    "plot()" function creates a pyplot object of the data
+        x_unit: str of the unit of concentration
+
+        y_unit: str of unit for signal, default is absorbance.
+
+        shape: modifies the order of experiments and replicates.
+
+        self.fit is a regression object (see scipy -> linreg for further details)
+
+    output:
+        printing the class shows matrix of data
+
+        "plot()" function creates a pyplot object of the data
     """
 
     def __init__(
@@ -76,6 +89,8 @@ class Standard(Absorbance):
         self.y_unit = y_unit
 
     def __repr__(self):
+
+        # generate a dataframe of mean abs and std for abs
         df = pd.DataFrame(
             {
                 self.x_unit: self.c,
@@ -84,12 +99,18 @@ class Standard(Absorbance):
             }
         )
 
+        # insert columns of replicate data into dataframe
+        for i, _ in enumerate(self.abs):
+            df.insert(i + 1, f"abs #{i+1}", self.abs[i, :])
+
         return f"Standard Data: \n {df}"
 
     def plot(self):
         """
-        shows the plot of the standard curve
+        create the plot object of the standard curve
         """
+
+        # data points
         plt.errorbar(
             self.c,
             self.mean,
@@ -100,16 +121,18 @@ class Standard(Absorbance):
             capsize=3,
             label="std data",
         )
+
+        # regression line
         plt.plot(
-            range(self.c[-1]),
-            range(self.c[-1]) * self.fit.slope + self.fit.intercept,
+            range(int(self.c[-1])),
+            range(int(self.c[-1])) * self.fit.slope + self.fit.intercept,
             color="green",
             linewidth=1,
             label=f"fit abs = {self.fit.slope:0.2f} * c + {self.fit.intercept:0.2f}",
         )
         plt.grid(True)
-        plt.xlabel(self.x_unit)
-        plt.ylabel(self.y_unit)
+        plt.xlabel(f"Concentration [{self.x_unit}]")
+        plt.ylabel(f"Absorbance [{self.y_unit}]")
         plt.legend()
         plt.title(f"Standard Curve - {str(date.today())}")
 
@@ -118,17 +141,18 @@ class Enzyme(Absorbance):
     """
     child Class for sample data, allows for calculation of concentration and activity based on std data.
 
-    INPUT:
+    input:
     absorbance: numpy array of your absorbance data, default is experiments in rows, replicates in col.
     Name: str of experiment name
     dilution_factor: numpy array of the dilution factors of each experiment.
+    time: time in [min]
     x_unit: str of the unit of concentration
     y_unit: str of unit for signal, default is absorbance.
     shape: modifies the order of experiments and replicates.
 
     concentration: numpy array of estimated concentrations.
 
-    OUTPUT:
+    output:
     printing the class shows matrix of data
     "plot()" function creates a pyplot object of the data
 
@@ -139,6 +163,7 @@ class Enzyme(Absorbance):
         absorbance: np.ndarray,
         Name: str,
         regr,
+        time: list[float],
         dilution_factor: np.ndarray = 1,
         x_unit: str = None,
         y_unit: str = "AU",
@@ -152,6 +177,9 @@ class Enzyme(Absorbance):
         self.x_unit = x_unit
         self.y_unit = y_unit
         self.dil = dilution_factor
+        self.time = time
+
+        # concentration of analyte in cuvette
         self.c = estimate(
             abs=self.mean, slope=self.regr.slope, intercept=self.regr.intercept
         )
@@ -159,22 +187,57 @@ class Enzyme(Absorbance):
             estimate(abs=self.std, slope=self.regr.slope, intercept=self.regr.intercept)
         )
 
+        # calculate activity in nkat/ml based on unit in "x_unit"
+        if self.x_unit == "nM":
+            # [nM] / 1000_000_000 [M/nM] * 1000_000_000 [nM/M] /1000 [l/ml] / 600 [1/s] = nmol/ml/s = nkat/ml
+            self.activity = self.c / 1000 / 600 * self.dil
+
+        elif self.x_unit == "μM" or "uM":
+            # [uM] / 1000_000 [M/uM] * 1000_000_000 [nM/M] /1000 [l/ml] / 600 [1/s] = nmol/ml/s = nkat/ml
+            self.activity = self.c * 1 / 600 * self.dil
+
+        elif self.x_unit == "mM":
+            # [mM] / 1000 [M/mM] * 1000_000_000 [nM/M] /1000 [l/ml] / 600 [1/s] = nmol/ml/s = nkat/ml
+            self.activity = self.c * 1_000 / 600 * self.dil
+
+        elif self.x_unit == "M":
+            # [M] * 1000_000_000 [nM/M] /1000 [l/ml] / 600 [1/s] = nmol/ml/s = nkat/ml
+            self.activity = self.c * 1_000_000 / 600 * self.dil
+
+        else:
+            print("Wrong unit for X-values, needs to be [nM],[μM],[mM], or [M]")
+            raise NotImplementedError
+
+        # if there are something in each cell of self.activity, calc. the std dev
+        if self.activity.all():
+            self.activity_std = abs(np.std(self.activity, axis=0)) * self.dil
+
     def __repr__(self):
+
+        # generate dataframe of mean abs, std, calculated concentrations, and std.
         df = pd.DataFrame(
             {
                 f"mean [{self.y_unit}]": self.mean,
                 f"+/- [{self.y_unit}]": self.std,
-                f"c [{self.x_unit}]": self.c,
+                f"conc. analyte [{self.x_unit}]": self.c,
                 f"+/- [{self.x_unit}]": self.c_std,
+                "activity [nkat/ml]": self.activity,
+                "+/- [nkat/ml]": self.activity_std,
             }
         )
+
+        # insert columns of replicate data into dataframe
+        for i, _ in enumerate(self.abs):
+            df.insert(i, f"abs #{i+1}", self.abs[i, :])
 
         return f"{self.name}: \n {df}"
 
     def plot(self) -> None:
         """
-        shows the plot of the standard curve
+        Create the plot object of the sample data.
         """
+
+        # data points for sample.
         plt.errorbar(
             self.c,
             self.mean,
@@ -211,13 +274,16 @@ def main():
         absorbance=[15, 12, 16] + np.random.rand(3, 1) * 4 + 3,
         regr=std.fit,
         dilution_factor=10,
+        time=60.0,
         x_unit=std.x_unit,
         y_unit=std.y_unit,
     )
 
-    # shows the data in matrix and graphical forms.
+    # shows the data in matrix form
     print(std)
     print(TrMan5A)
+
+    # shows the data in plot window
     TrMan5A.plot()
     std.plot()
     plt.show()
--- utils/maldi_tof.py
+++ utils/maldi_tof.py
@@ -24,3 +24,32 @@ def load_ms_csv(path: str):
                 {"mz": mz, "signal": signal}
             )
     return data
+
+    def plot_ms(plots, check=None):
+    """
+    Hej
+    """
+    f, axs = plt.subplots(len(plots), 1, figsize=(10, 8), sharex=True, sharey=True)
+    for i, plot in enumerate(plots):
+
+        mz = df[plot]["mz"]
+        signal = df[plot]["signal"]
+
+        if len(plots) > 1:
+            ax = f.add_subplot(axs[i])
+
+        if check:
+            for line in check:
+                plot_vert(line)
+
+        plt.plot(mz, signal, "-", color="black", linewidth=1)
+        plt.xlabel("m/z")
+        plt.ylabel("Signal")
+        plt.xlim([200, 400])
+        # plt.xlim([300, 370])
+        plt.ylim([0, max(signal) * 1.05])
+        plt.grid()
+        plt.legend({plot}, loc="best")
+
+    plt.suptitle("Mass Spectrum - %s" % date.today())
+    plt.show()
